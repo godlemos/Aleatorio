b5f3c73052cd7d239e97a0f93d40b82694703628
Carlos Lemos <lemosss2003@gmail.com>
2025-01-26 19:59:50 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aleatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejercicio 1: Adivinar un número aleatorio entre 1 y 100
+            Random rand = new Random();
+            int numeroAleatorio = rand.Next(1, 101); // Genera un número entre 1 y 100
+            int intento;
+            do
+            {
+                Console.Write("Adivina el número (entre 1 y 100): ");
+                intento = int.Parse(Console.ReadLine());
+
+                if (intento < numeroAleatorio)
+                {
+                    Console.WriteLine("El número es mayor.");
+                }
+                else if (intento > numeroAleatorio)
+                {
+                    Console.WriteLine("El número es menor.");
+                }
+                else
+                {
+                    Console.WriteLine("¡Adivinaste el número!");
+                }
+            } while (intento != numeroAleatorio);
+
+            Console.WriteLine("¡Felicidades! Has adivinado el número correctamente.");
+        }
+    }
+}
